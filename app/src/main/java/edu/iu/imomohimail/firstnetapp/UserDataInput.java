5aa6d8f49bab3702e74224b1c6fcd166fbf5e0cc
Chris Michel <cmich007@fiu.edu>
2019-09-14 01:02:33 -0500

    Fragment consent form

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
@@ -2,9 +2,19 @@ package edu.iu.imomohimail.firstnetapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserDataInput extends AppCompatActivity {
 
+    String allergies, medications, surgicalHistory, preferredHospital;
+    int age, emergencyContactNumm;
+
+
+
+
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/consentForm.java
@@ -0,0 +1,28 @@
+package edu.iu.imomohimail.firstnetapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class consentForm extends Fragment {
+
+    public consentForm() {
+        // Required empty public constructor
+    }
+    
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_consent_form, container, false);
+    }
+
+
+}
