036a321249c250792da4d2a74baceba58a56f213
Ibrahim Momoh <imomoh@imail.iu.edu>
2019-09-14 00:05:37 -0400

    color added

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/MainActivity.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/MainActivity.java
@@ -1,13 +1,36 @@
 package edu.iu.imomohimail.firstnetapp;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    public static boolean isUser= false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initiate a Switch
+        final ConstraintLayout  mainvIEW = (ConstraintLayout) findViewById(R.id.screen1back);
+        Switch simpleSwitch = (Switch) findViewById(R.id.usersettings);
+        mainvIEW.setBackgroundColor(getResources().getColor(R.color.blue));
+// check current state of a Switch (true or false).
+        Boolean switchState = simpleSwitch.isChecked();
+        simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b){
+                        mainvIEW.setBackgroundColor(getResources().getColor(R.color.red));
+                }else {
+                    mainvIEW.setBackgroundColor(getResources().getColor(R.color.blue));
+                }
+            }
+        });
     }
 }
