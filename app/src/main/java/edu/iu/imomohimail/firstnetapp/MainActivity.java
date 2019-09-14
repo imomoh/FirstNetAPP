3f126abf664eb9b6fae3138da618af17d3b63227
Ibrahim Momoh <imomoh@imail.iu.edu>
2019-09-14 00:47:47 -0400

    added on click

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/MainActivity.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/MainActivity.java
@@ -1,24 +1,31 @@
 package edu.iu.imomohimail.firstnetapp;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static boolean isUser= false;
-
+    ConstraintLayout  mainvIEW;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // initiate a Switch
-        final ConstraintLayout  mainvIEW = (ConstraintLayout) findViewById(R.id.screen1back);
+        mainvIEW = (ConstraintLayout) findViewById(R.id.screen1back);
+        CardView cardView = findViewById(R.id.findoption);
         Switch simpleSwitch = (Switch) findViewById(R.id.usersettings);
         mainvIEW.setBackgroundColor(getResources().getColor(R.color.blue));
+        final TextView UserType = (TextView) findViewById(R.id.usertypetextview);
+        UserType.setText("Citizen");
 // check current state of a Switch (true or false).
         Boolean switchState = simpleSwitch.isChecked();
         simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -27,11 +34,23 @@ public class MainActivity extends AppCompatActivity {
 
                 if(b){
                         mainvIEW.setBackgroundColor(getResources().getColor(R.color.red));
+                    UserType.setText("First Responder");
 
                 }else {
                     mainvIEW.setBackgroundColor(getResources().getColor(R.color.blue));
+
+                    UserType.setText("Citizen");
                 }
             }
         });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cardView.setOnContextClickListener(new View.OnContextClickListener() {
+                @Override
+                public boolean onContextClick(View view) {
+
+                    return false;
+                }
+            });
+        }
     }
 }
