6b44f2f787bf585fd94a0d1c0e6a8b4f91123223
Chris Michel <cmich007@fiu.edu>
2019-09-14 02:24:37 -0500

    Good night bro

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/consentForm.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/consentForm.java
@@ -7,7 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class consentForm extends Fragment {
@@ -21,6 +21,7 @@ public class consentForm extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_consent_form, container, false);
     }
 
