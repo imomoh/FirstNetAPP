77d1206762aca2f4961c36c9e1bbfbd2a29ad988
Chris Michel <cmich007@fiu.edu>
2019-09-14 02:13:26 -0500

    Input info page to consent page

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
@@ -1,7 +1,9 @@
 package edu.iu.imomohimail.firstnetapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,5 +21,14 @@ public class UserDataInput extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_data_input);
+
+        Button submitButton = findViewById(R.id.addButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserDataInput.this, consentForm.class));
+            }
+        });
     }
 }
