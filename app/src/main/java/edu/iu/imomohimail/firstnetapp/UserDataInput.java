27227646b5509d425b201dd326619b1561c6a5bd
Chris Michel <cmich007@fiu.edu>
2019-09-14 12:45:50 -0500

    set methods

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 
 public class UserDataInput extends AppCompatActivity {
 
-    String allergies, medications, surgicalHistory, preferredHospital;
+    String driverLicenseNum, allergies, medications, preferredHospital;
     int age, emergencyContactNumm;
 
-
+    userInformation user;
 
 
     Button submitButton;
@@ -22,11 +22,30 @@ public class UserDataInput extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_data_input);
 
+
+
         Button submitButton = findViewById(R.id.addButton);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText driverLicenseNumInfo = findViewById(R.id.driverLicenseNumber);
+                EditText ageInfo = findViewById(R.id.ageInput);
+                EditText allergiesInfo = findViewById(R.id.allergiesInput);
+                EditText medicationsInfo = findViewById(R.id.medicationInput);
+                EditText medicalConditionsInfo = findViewById(R.id.conditionsInput);
+                EditText emergencyContactInfo = findViewById(R.id.emergencyContactInput);
+
+                user = new userInformation();
+
+                user.setUser_driver_license_number(String.valueOf(driverLicenseNumInfo.getText()));
+                user.setAge(String.valueOf(ageInfo));
+                user.setAllergies(allergiesInfo.toString());
+                user.setMedications(String.valueOf(medicationsInfo));
+                user.setMedidcal_conditions(String.valueOf(medicalConditionsInfo));
+                user.setEmergency_contact_phone_number(String.valueOf(emergencyContactInfo));
+
+
                 startActivity(new Intent(UserDataInput.this, consentForm.class));
             }
         });
