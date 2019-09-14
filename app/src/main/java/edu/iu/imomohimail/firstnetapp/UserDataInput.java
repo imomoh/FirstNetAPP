77ba75281d1896edb78d5945b515d85cc65cce6e
Chris Michel <cmich007@fiu.edu>
2019-09-14 13:12:01 -0500

    Allow main thread

--- app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/UserDataInput.java
@@ -7,22 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.room.Room;
 
 public class UserDataInput extends AppCompatActivity {
 
     String driverLicenseNum, allergies, medications, preferredHospital;
     int age, emergencyContactNumm;
 
+    public static AppDataBase myAppDataBase;
     userInformation user;
+    UserInformationDao myDao;
 
-
-    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_data_input);
 
-
+        myAppDataBase = Room.databaseBuilder(getApplicationContext(), AppDataBase.class,  "health_infodb").allowMainThreadQueries().build();
 
         Button submitButton = findViewById(R.id.addButton);
 
@@ -44,6 +47,23 @@ public class UserDataInput extends AppCompatActivity {
                 user.setMedications(String.valueOf(medicationsInfo));
                 user.setMedidcal_conditions(String.valueOf(medicalConditionsInfo));
                 user.setEmergency_contact_phone_number(String.valueOf(emergencyContactInfo));
+
+
+                myDao.addUser(user);
+
+
+                CharSequence text = "Info Stored!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                toast.show();
+
+                driverLicenseNumInfo.setText("");
+                ageInfo.setText("");
+                allergiesInfo.setText("");
+                medicationsInfo.setText("");
+                medicalConditionsInfo.setText("");
+                emergencyContactInfo.setText("");
 
 
                 startActivity(new Intent(UserDataInput.this, consentForm.class));
