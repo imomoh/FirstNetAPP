cdd114d0149b35d2b23e50c7e8c9582f136bff87
Ibrahim Momoh <imomoh@imail.iu.edu>
2019-09-14 02:40:49 -0400

    emt side

---
+++ app/src/main/java/edu/iu/imomohimail/firstnetapp/FirstResponderProfile.java
@@ -0,0 +1,30 @@
+package edu.iu.imomohimail.firstnetapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstResponderProfile extends Fragment {
+
+
+    public FirstResponderProfile() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view= inflater.inflate(R.layout.fragment_first_responder_profile, container, false);
+       return view;
+    }
+
+}
